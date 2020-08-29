15013fa057931cbf44f8584bfef1261ee718a40f
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2020-08-29 11:57:52 +0600

    First commit

---
+++ CalculatorBySwift/Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by RokonuzzamanReza on 29/8/20.
+//  Copyright © 2020 RokonuzzamanReza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CalculatorBySwift/Calculator/viewController.swift
@@ -0,0 +1,21 @@
+//
+//  viewController.swift
+//  Calculator
+//
+//  Created by RokonuzzamanReza on 29/8/20.
+//  Copyright © 2020 RokonuzzamanReza. All rights reserved.
+//
+
+import SwiftUI
+
+struct viewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct viewController_Previews: PreviewProvider {
+    static var previews: some View {
+        viewController()
+    }
+}
